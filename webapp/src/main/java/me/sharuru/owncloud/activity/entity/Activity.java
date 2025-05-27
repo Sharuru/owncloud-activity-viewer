d9ad8e45b086ecae17e78b0f366c1003c46f5758
Sharuru <mave@foxmail.com>
2025-05-27 18:11:17 +0800

    update the dependency

--- webapp/src/main/java/me/sharuru/owncloud/activity/entity/Activity.java
+++ webapp/src/main/java/me/sharuru/owncloud/activity/entity/Activity.java
@@ -4,10 +4,10 @@ import com.fasterxml.jackson.annotation.JsonView;
 import lombok.Data;
 import org.springframework.data.jpa.datatables.mapping.DataTablesOutput;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.io.Serial;
 import java.io.Serializable;
 
